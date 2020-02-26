54fa86d9b401626282c5f37ef455f30e5c2ad285
woojoo <woojoo@softmarshmallow.com>
2020-02-27 06:55:24 +0900

    updated shell

--- crawler.sh
+++ crawler.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source venv/bin/activate
-venv/bin/python -m pip install -r requirements.txt
+./venv/bin/python -m pip install -r requirements.txt
 export PYTHONPATH="${PYTHONPATH}:inkedNewsCrawler"
-venv/bin/python inkedNewsCrawler/custom_crawler/naver_news_crawler/live_crawler.py
+./venv/bin/python inkedNewsCrawler/custom_crawler/naver_news_crawler/live_crawler.py
